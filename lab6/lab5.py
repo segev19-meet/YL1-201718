87db292f10b92eb1f1c565fb6c2614e971c97b54
segev19-meet <segev19@meet.mit.edu>
2017-11-22 19:24:51 +0200

    segev work

---
+++ lab6/lab5.py
@@ -0,0 +1,36 @@
+##from turtle import *
+##colormode(255)
+##class Square(Turtle):
+##    def __init__(self,size):
+##        Turtle.__init__(self)
+##        self.shapesize(size)
+##        self.shape("square")
+##
+##s1 = Square(5)
+
+
+##from turtle import *
+##colormode(255)
+##class Hexagon(Turtle):
+##    def __init__(self,size):
+##        turtle.__init__(self)
+##        self.shapesize(size)
+##        self.shape("hexagon")
+##
+##s1=Hexagon(5)        
+
+import turtle
+turtle.forward(200)
+turtle.left(40)
+turtle.forward(100)
+turtle.left(80)
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(200)
+
+
+
+
+
+
+turtle.mainloop
